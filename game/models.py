31986e83342ae6ce5354adbc2e9831a073d19354
karamyanes <karamyanes@hotmail.com>
2021-03-08 18:51:01 +0100

    new_database

--- game/models.py
+++ game/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.base import Model
 
 
 class Lobby(models.Model):
@@ -19,8 +20,6 @@ class Question(models.Model):
     )
     question_text = models.CharField(max_length=2000)
     correct_answer = models.CharField(max_length=100,choices=STATUS)
-    game_id = models.ForeignKey(Lobby, default=0, on_delete=models.CASCADE, verbose_name = "related to Lobby")
-    
     
     def __str__(self):
         return self.question_text
@@ -34,9 +33,9 @@ class Answer(models.Model):
         ('mostly-true','mostly-true'),
         ('pants-fire','pants-fire'),
     )
-    questionid=models.ForeignKey(Question, on_delete=models.CASCADE, verbose_name = "related to Question")
-    answer_text= models.CharField(max_length=256,choices=STATUS)
-    is_correct=models.BooleanField(default=False)
+    questionid = models.ForeignKey(Question, on_delete=models.CASCADE, verbose_name = "related to Question")
+    answer_text = models.CharField(max_length=256,choices=STATUS)
+    is_correct = models.BooleanField(default=False)
 
     def __str__(self):
         return self.answer_text
@@ -52,18 +51,23 @@ class Player(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     score = models.IntegerField(default=0, null=True, blank=True)
     game_id = models.ForeignKey(Lobby, default=0, on_delete=models.CASCADE, verbose_name = "related to Lobby")
-    user_status= models.CharField(max_length=256,choices=Status)
-
+    user_status = models.CharField(max_length=256,choices=Status)
+    
     def str(self):
         return self.user.username
     
 class UserQuestionHistory(models.Model):
-    questioner_id= models.ForeignKey(Player, on_delete=models.CASCADE, related_name = "questioner")
-    respoender_id= models.ForeignKey(Player, on_delete=models.CASCADE, related_name = "respoender")
+    questioner_id = models.ForeignKey(Player, on_delete=models.CASCADE, related_name = "questioner")
+    respoender_id = models.ForeignKey(Player, on_delete=models.CASCADE, related_name = "respoender")
 
 
 class Result(models.Model):
-    answer_id= models.ForeignKey(Answer, on_delete=models.CASCADE, related_name = "Answer") 
-    question_id= models.ForeignKey(Question, on_delete=models.CASCADE, related_name = "spørsmål")
-    questioner_id= models.ForeignKey(Player, on_delete=models.CASCADE, related_name = "Player_1")
-    respondent_id=models.ForeignKey(Player, on_delete=models.CASCADE, related_name = "Player_2")
+    answer_id = models.ForeignKey(Answer, on_delete=models.CASCADE, related_name = "Answer") 
+    question_id = models.ForeignKey(Question, on_delete=models.CASCADE, related_name = "spørsmål")
+    questioner_id = models.ForeignKey(Player, on_delete=models.CASCADE, related_name = "Player_1")
+    respondent_id = models.ForeignKey(Player, on_delete=models.CASCADE, related_name = "Player_2")
+
+
+class GameQuestions(models.Model):
+    question_id = models.ForeignKey(Question, on_delete=models.CASCADE, related_name = "Question")
+    game_id = models.ForeignKey(Lobby, on_delete=models.CASCADE, verbose_name = "related to Lobby")
