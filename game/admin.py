e7b2f2801af8146e1c390c879ae421609b4cf4cb
karamyanes <karamyanes@hotmail.com>
2021-03-22 15:04:33 +0100

    update admin.py

--- game/admin.py
+++ game/admin.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
-from .models import Answer, Lobby
-from .models import Question
-from .models import Player
-from .models import Lobby
+from .models import Answer, Lobby, Question, Player, LobbyQuestion
 
 
 admin.site.register(Answer)
 admin.site.register(Question)
 admin.site.register(Player)
 admin.site.register(Lobby)
+admin.site.register(LobbyQuestion)
